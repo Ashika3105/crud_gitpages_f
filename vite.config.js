70b2ef79b60b6a8496364d2c7ef459a743080b88
Ashika3105 <142997958+Ashika3105@users.noreply.github.com>
2025-10-03 00:09:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'crud_gitpages_f',
+  base: '/crud_gitpages_f/',
 })
